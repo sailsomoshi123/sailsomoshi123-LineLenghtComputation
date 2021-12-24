bb79095a45d848e4db65c9deb6b93e74776e3dce
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-24 07:21:22 +0530

    welcome massage

---
+++ LineLenghtComputation.java
@@ -0,0 +1,7 @@
+package com.linelLenghtComputation;
+
+public class LineLenghtComputation {
+	public static void main(String[] args) {
+		System.out.println("welcome to line lenght computation problem");
+	}
+}
