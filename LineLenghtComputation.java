21db3f78ab98b31a92f02c6cf71b39f7683f5b11
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-24 07:49:35 +0530

    second usecase

--- LineLenghtComputation.java
+++ LineLenghtComputation.java
@@ -12,11 +12,34 @@ public class LineLenghtComputation {
 		int x2 = sc.nextInt();
 		System.out.println("Enter y-cordinate of second point: ");
 		int y2 = sc.nextInt();
-		sc.close();
 		
 		
 		//Calculation of lenght of line
-		double Line_Lenght = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
-		System.out.println("Lenght of line : "+ Line_Lenght);
+		double Line_Lenght1 = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+		System.out.println("Lenght of line : "+ Line_Lenght1);
+		
+		//Co-ordinates for second line
+		System.out.println("Enter Co-ordinates for second line");
+				
+		System.out.println("Enter x-cordinate of first point: ");
+		int px1 = sc.nextInt(); 
+		System.out.println("Enter y-cordinate of first point: ");
+		int py1 = sc.nextInt();
+		System.out.println("Enter x-cordinate of second point: ");
+		int px2 = sc.nextInt();
+		System.out.println("Enter y-cordinate of second point: ");
+		int py2 = sc.nextInt();
+				
+		double Line_Lenght2 = Math.sqrt( (px2 - px1) ^ 2 + (py2- py1) ^ 2);
+	    System.out.println("Lenght of line : "+ Line_Lenght2);
+	    
+	    if( Line_Lenght2== Line_Lenght1  ) {
+			System.out.println("\nBoth lines are equal!");
+			
+		}
+		else {
+			System.out.println("\nLines are Distinct!");
+		}
+	 
 	}
 }
